e13d4b7f9d9e932243a3d5075e071cba358ab70b
James Dabbs <jamesdabbs@gmail.com>
2015-06-24 09:52:02 -0400

    Review scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :questions
   resources :items
 
   root 'items#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623140926) do
+ActiveRecord::Schema.define(version: 20150624132653) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150623140926) do
     t.integer  "quantity_available"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
